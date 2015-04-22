2d1a9960fba6edce9e7349b2ecf14bc725d529b2
casperchen(陈映平) <chyingp@gmail.com>
2015-04-23 00:11:04 +0800

    添加gzip压缩

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express'),
 	app = express(),
-	router = require('./routers');
+	router = require('./routers'),
+	compression = require('compression');
 
 app.set('root', './');
+app.use(compression({threshold: 0}));	// gzip
 app.use('/c/=/', router);
 app.use(express.static(__dirname + '/public'));
 app.listen(3000);
